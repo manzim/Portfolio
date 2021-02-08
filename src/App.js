f658d87cd48d4704f7859b1c64823b530fe5a4b7
manzim-doctorkoi <manzim.doctorkoi@gmail.com>
2021-02-08 23:43:12 +0600

    initial commit & navBar done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { MyNavbar } from "../src/components/my-navbar/mynavbar.jsx";
+
+function App() {
+  return (
+    <>
+    <MyNavbar />
+    <div className="App">
+      my portfolio
+    </div>
+    </>
+  );
+}
+
+export default App;

d340a6342f210442ea7db2b2591b22af70f3d34f
manzim-doctorkoi <manzim.doctorkoi@gmail.com>
2021-02-09 23:41:19 +0600

    scroll btn, slider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import { MyNavbar } from "../src/components/my-navbar/mynavbar.jsx";
+import { MyCarousal } from './components/my-carousal/mycarousal.jsx';
+import { MyTitle } from './components/title-msg/mytitlemsg';
 
 function App() {
   return (
     <>
     <MyNavbar />
-    <div className="App">
-      my portfolio
-    </div>
+    <MyCarousal />
+    <MyTitle />
     </>
   );
 }

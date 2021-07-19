6873171f1bfac08be7762ddc7b909c83ec46b2da
manzim <manzim.ruet@gmail.com>
2021-07-19 15:28:57 +0600

    navBar correction

--- src/components/my-navbar/mynavbar.jsx
+++ src/components/my-navbar/mynavbar.jsx
@@ -3,29 +3,32 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown'
 
-import Logo from "../../assets/icons/brandicon.png";
+import Logo from "../../assets/icons/mybrandlogo.png";
 
 import './mynavbar.css'
 
 export function MyNavbar() {
     return (
         <>
-            <Navbar fixed="top" collapseOnSelect expand="md" variant="dark" className="animate-navbar nav-theme justify-content-between">
+            <Navbar
+                fixed="top"
+                collapseOnSelect
+                expand="md"
+                variant="dark"
+                className="animate-navbar nav-theme justify-content-between"
+            >
                 <Navbar.Brand href="#home">
-                    <img className="logo" src={Logo} alt="my brand logo" />
+                    <img className="logo" src={Logo} alt="my brand" />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="ml-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#about">About</Nav.Link>
                         <Nav.Link href="#skills">Skills</Nav.Link>
                         <Nav.Link href="#experiences">Experiences</Nav.Link>
                         <Nav.Link href="#projects">Projects</Nav.Link>
-                        <Nav.Link href="#education">Education</Nav.Link>
-                        <Nav.Link href="#contact">Contact</Nav.Link>
-                        <NavDropdown  title="Others" id="nav-dropdown" aria-controls="responsive-navbar-nav" >
-                            <NavDropdown.Item target="_blank" href="https://gaming.uefa.com/en/uefaeuro2020fantasyfootball/" >Fantasy Premier League</NavDropdown.Item>
+                        <NavDropdown title="Others" id="nav-dropdown" aria-controls="responsive-navbar-nav" >
+                            <NavDropdown.Item target="_blank" href="" >Fantasy Premier League</NavDropdown.Item>
                             <NavDropdown.Divider />
                             <NavDropdown.Item target="_blank" href="" >Coding Stories</NavDropdown.Item>
                             <NavDropdown.Divider />
@@ -33,6 +36,9 @@ export function MyNavbar() {
                             <NavDropdown.Divider />
                             {/* <NavDropdown.Item >Separated link</NavDropdown.Item> */}
                         </NavDropdown>
+                        {/* <Nav.Link href="#education">Education</Nav.Link> */}
+                        <Nav.Link href="#contact">Contact</Nav.Link>
+                        <Nav.Link href="#about">About</Nav.Link>                        
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
